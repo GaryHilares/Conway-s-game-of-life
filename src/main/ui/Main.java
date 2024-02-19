e7b9d91e34ce9a4d4e9bb1fd999af3184dbbe2fd
Gary Hilares <dev.garyhilares@gmail.com>
2024-02-19 03:04:33 -0800

    Added effects clause to Main.main function.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -2,6 +2,7 @@ package ui;
 
 // Initializes and runs a new command-line interface of the Conway's game of life simulator.
 public class Main {
+    // EFFECTS: Setups and runs a new command-line interface of Conway's game of life simulator.
     public static void main(String[] args) {
         GameOfLifeCli cli = new GameOfLifeCli();
         cli.setup();

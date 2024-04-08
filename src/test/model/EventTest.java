16f918690d382800930828cd7528c69d121160e7
Gary Hilares <dev.garyhilares@gmail.com>
2024-04-08 03:06:40 -0700

    Reduced sensitivity of Event test so that it runs in slower computers

--- src/test/model/EventTest.java
+++ src/test/model/EventTest.java
@@ -18,7 +18,7 @@ public class EventTest {
 
     //NOTE: these tests might fail if time at which line (2) below is executed
     //is different from time that line (1) is executed.  Lines (1) and (2) must
-    //run in same millisecond for this test to make sense and pass.
+    //run in same second for this test to make sense and pass.
 
     @BeforeEach
     public void runBefore() {
@@ -29,7 +29,7 @@ public class EventTest {
     @Test
     public void testEvent() {
         assertEquals("Sensor open at door", e.getDescription());
-        assertEquals(d, e.getDate());
+        assertEquals(d.toString(), e.getDate().toString());
     }
 
     @Test
